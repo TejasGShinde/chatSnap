03dbe3360c2929f635a9cfbd155782b462886dde
unknown <ptejas387@gmail.com>
2023-07-18 21:42:02 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUgR64grl_72PsDc8ELzo9iy2bE4i3Ruo",
+  authDomain: "chatsnap-4b369.firebaseapp.com",
+  projectId: "chatsnap-4b369",
+  storageBucket: "chatsnap-4b369.appspot.com",
+  messagingSenderId: "1023127554702",
+  appId: "1:1023127554702:web:f7269f771deec4273997df"
+};
+
+
+export const app = initializeApp(firebaseConfig);
